6d116f920f792d92c1d7a564d9e0b8c6c52443d1
Daniel Lutz <daniel.lutz@outlook.com>
2020-06-12 10:00:07 +0200

    fix ressources

--- src/AddCitaviProjectToRecentListAddon/Addon.cs
+++ src/AddCitaviProjectToRecentListAddon/Addon.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿using AddCitaviProjectToRecentList.Properties;
+using SwissAcademic.Citavi.Shell;
+using System;
 using System.Linq;
 using System.Windows.Forms;
-using SwissAcademic.Citavi.Shell;
 
 namespace AddCitaviProjectToRecentList
 {
     public class Addon : CitaviAddOn<MainForm>
     {
-        #region Methods
-
         public override void OnHostingFormLoaded(MainForm mainForm)
         {
             var project = Program.Engine.Settings.General.KnownProjects.FirstOrDefault();
@@ -21,13 +20,11 @@ namespace AddCitaviProjectToRecentList
                 }
                 catch (Exception e)
                 {
-                    MessageBox.Show(mainForm, e.ToString(), "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show(mainForm, e.ToString(), Resources.Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
 
             base.OnHostingFormLoaded(mainForm);
         }
-
-        #endregion
     }
 }
