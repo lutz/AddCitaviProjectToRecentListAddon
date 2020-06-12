5afdc0455db73119ae42bf5ade4e1e04c2b9f878
Daniel Lutz <daniel.lutz@outlook.com>
2020-06-12 10:02:22 +0200

    remove regions

--- src/AddCitaviProjectToRecentListAddon/Core/Shell32Api.cs
+++ src/AddCitaviProjectToRecentListAddon/Core/Shell32Api.cs
@@ -5,17 +5,11 @@ namespace AddCitaviProjectToRecentList
 {
     public class Shell32Api
     {
-        #region Nested Elements
-
         public enum ShellAddToRecentDocsFlags
         {
             Pidl = 0x001,
             Path = 0x002,
         }
-
-        #endregion
-
-        #region Methods
 
         [DllImport("shell32.dll", CharSet = CharSet.Ansi)]
         public static extern void SHAddToRecentDocs(ShellAddToRecentDocsFlags flag, string path);
@@ -23,11 +17,6 @@ namespace AddCitaviProjectToRecentList
         [DllImport("shell32.dll")]
         public static extern void SHAddToRecentDocs(ShellAddToRecentDocsFlags flag, IntPtr pidl);
 
-        public static void AddToRecentDocs(string path)
-        {
-            SHAddToRecentDocs(ShellAddToRecentDocsFlags.Path, path);
-        }
-
-        #endregion
+        public static void AddToRecentDocs(string path) => SHAddToRecentDocs(ShellAddToRecentDocsFlags.Path, path);
     }
 }
